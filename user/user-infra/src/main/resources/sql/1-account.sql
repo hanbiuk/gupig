87014053abd5bc7f8a627417b007d3f23dfa2d6c
hanbiuk <30977882+hanbiuk@users.noreply.github.com>
2024-12-09 15:26:54 +0800

    docs: user - mysql integer display width is deprecated

--- user/user-infra/src/main/resources/sql/1-account.sql
+++ user/user-infra/src/main/resources/sql/1-account.sql
@@ -1,7 +1,7 @@
 -- 1. 用户账号表
 CREATE TABLE `user_account`
 (
-    `id`          bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键编号',
+    `id`          bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键编号',
     `code`        varchar(32)  NOT NULL DEFAULT '' COMMENT '主键编码',
     `tenant_code` varchar(32)  NOT NULL DEFAULT '' COMMENT '租户编码',
     `username`    varchar(32)  NOT NULL DEFAULT '' COMMENT '用户名',
@@ -11,11 +11,11 @@ CREATE TABLE `user_account`
     `salt`        varchar(32)  NOT NULL DEFAULT '' COMMENT '密码加盐',
     `email`       varchar(64)  NOT NULL DEFAULT '' COMMENT '邮箱',
     `mobile`      varchar(64)  NOT NULL DEFAULT '' COMMENT '手机号',
-    `gender`      tinyint(4)   NOT NULL DEFAULT '0' COMMENT '性别: 0-未知, 1-男, 2-女',
+    `gender`      tinyint      NOT NULL DEFAULT '0' COMMENT '性别: 0-未知, 1-男, 2-女',
     `birthday`    date         NOT NULL DEFAULT '2000-01-01' COMMENT '出生年月日',
     `portrait`    varchar(255) NOT NULL DEFAULT '' COMMENT '头像',
-    `status`      tinyint(4)   NOT NULL DEFAULT '0' COMMENT '状态: 1-启用, 2-禁用, 3-销号',
-    `is_deleted`  tinyint(4)   NOT NULL DEFAULT '0' COMMENT '是否删除: 0-否, 1-是',
+    `status`      tinyint      NOT NULL DEFAULT '0' COMMENT '状态: 1-启用, 2-禁用, 3-销号',
+    `is_deleted`  tinyint      NOT NULL DEFAULT '0' COMMENT '是否删除: 0-否, 1-是',
     `creator`     varchar(32)  NOT NULL DEFAULT '' COMMENT '创建人',
     `cst_create`  datetime     NOT NULL DEFAULT '2000-01-01 00:00:01' COMMENT '创建时间',
     `modifier`    varchar(32)  NOT NULL DEFAULT '' COMMENT '修改人',
@@ -31,7 +31,7 @@ CREATE TABLE `user_account`
 -- 2. 用户账号业务线表
 CREATE TABLE `user_account_biz`
 (
-    `id`          bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键编号',
+    `id`          bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键编号',
     `code`        varchar(32)  NOT NULL DEFAULT '' COMMENT '主键编码',
     `tenant_code` varchar(32)  NOT NULL DEFAULT '' COMMENT '租户编码',
     `ua_code`     varchar(32)  NOT NULL DEFAULT '' COMMENT '用户账号编码',
@@ -39,8 +39,8 @@ CREATE TABLE `user_account_biz`
     `nickname`    varchar(32)  NOT NULL DEFAULT '' COMMENT '昵称',
     `portrait`    varchar(255) NOT NULL DEFAULT '' COMMENT '头像',
     `user_detail` varchar(256) NOT NULL DEFAULT '' COMMENT '自我简介/签名',
-    `status`      tinyint(4)   NOT NULL DEFAULT '1' COMMENT '状态: 1-启用, 2-禁用, 3-销号',
-    `is_deleted`  tinyint(4)   NOT NULL DEFAULT '0' COMMENT '是否删除: 0-否, 1-是',
+    `status`      tinyint      NOT NULL DEFAULT '1' COMMENT '状态: 1-启用, 2-禁用, 3-销号',
+    `is_deleted`  tinyint      NOT NULL DEFAULT '0' COMMENT '是否删除: 0-否, 1-是',
     `creator`     varchar(32)  NOT NULL DEFAULT '' COMMENT '创建人',
     `cst_create`  datetime     NOT NULL DEFAULT '2000-01-01 00:00:01' COMMENT '创建时间',
     `modifier`    varchar(32)  NOT NULL DEFAULT '' COMMENT '修改人',
@@ -55,14 +55,14 @@ CREATE TABLE `user_account_biz`
 -- 3. 用户账号登出表
 CREATE TABLE `user_account_logout`
 (
-    `id`          bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键编号',
+    `id`          bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键编号',
     `code`        varchar(32)  NOT NULL DEFAULT '' COMMENT '主键编码',
     `tenant_code` varchar(32)  NOT NULL DEFAULT '' COMMENT '租户编码',
     `ua_code`     varchar(32)  NOT NULL DEFAULT '' COMMENT '用户账号编码',
     `biz_code`    varchar(32)  NOT NULL DEFAULT '' COMMENT '业务线',
     `token`       varchar(512) NOT NULL DEFAULT '' COMMENT '登陆凭证',
     `cst_expire`  datetime     NOT NULL DEFAULT '2000-01-01 00:00:01' COMMENT '过期时间',
-    `is_deleted`  tinyint(4)   NOT NULL DEFAULT '0' COMMENT '是否删除: 0-否, 1-是',
+    `is_deleted`  tinyint      NOT NULL DEFAULT '0' COMMENT '是否删除: 0-否, 1-是',
     `creator`     varchar(32)  NOT NULL DEFAULT '' COMMENT '创建人',
     `cst_create`  datetime     NOT NULL DEFAULT '2000-01-01 00:00:01' COMMENT '创建时间',
     `modifier`    varchar(32)  NOT NULL DEFAULT '' COMMENT '修改人',
--- user/user-infra/src/main/resources/sql/2-notice.sql
+++ user/user-infra/src/main/resources/sql/2-notice.sql
@@ -1,25 +1,25 @@
 -- 1. 用户通知验证码表
 CREATE TABLE `user_notice_verification`
 (
-    `id`            bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键编号',
-    `code`          varchar(32)         NOT NULL DEFAULT '' COMMENT '主键编码',
-    `tenant_code`   varchar(32)         NOT NULL DEFAULT '' COMMENT '租户编码',
-    `biz_code`      varchar(32)         NOT NULL DEFAULT '' COMMENT '业务线',
-    `function_code` varchar(32)         NOT NULL DEFAULT '' COMMENT '场景',
-    `touch_means`   varchar(32)         NOT NULL DEFAULT '' COMMENT '触达方式',
-    `user_identity` varchar(32)         NOT NULL DEFAULT '' COMMENT '用户身份',
-    `request_code`  varchar(32)         NOT NULL DEFAULT '' COMMENT '请求编码',
-    `verification`  varchar(32)         NOT NULL DEFAULT '' COMMENT '验证码',
-    `cst_expire`    datetime            NOT NULL DEFAULT '9999-12-31 23:59:59' COMMENT '过期时间',
-    `is_deleted`    tinyint(4)          NOT NULL DEFAULT '0' COMMENT '是否删除: 0-否, 1-是',
-    `creator`       varchar(32)         NOT NULL DEFAULT '' COMMENT '创建人',
-    `cst_create`    datetime            NOT NULL DEFAULT '2000-01-01 00:00:01' COMMENT '创建时间',
-    `modifier`      varchar(32)         NOT NULL DEFAULT '' COMMENT '修改人',
-    `cst_modify`    datetime            NOT NULL DEFAULT '2000-01-01 00:00:01' COMMENT '修改时间',
+    `id`            bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键编号',
+    `code`          varchar(32) NOT NULL DEFAULT '' COMMENT '主键编码',
+    `tenant_code`   varchar(32) NOT NULL DEFAULT '' COMMENT '租户编码',
+    `biz_code`      varchar(32) NOT NULL DEFAULT '' COMMENT '业务线',
+    `function_code` varchar(32) NOT NULL DEFAULT '' COMMENT '场景',
+    `touch_means`   varchar(32) NOT NULL DEFAULT '' COMMENT '触达方式',
+    `user_identity` varchar(32) NOT NULL DEFAULT '' COMMENT '用户身份',
+    `request_code`  varchar(32) NOT NULL DEFAULT '' COMMENT '请求编码',
+    `verification`  varchar(32) NOT NULL DEFAULT '' COMMENT '验证码',
+    `cst_expire`    datetime    NOT NULL DEFAULT '9999-12-31 23:59:59' COMMENT '过期时间',
+    `is_deleted`    tinyint     NOT NULL DEFAULT '0' COMMENT '是否删除: 0-否, 1-是',
+    `creator`       varchar(32) NOT NULL DEFAULT '' COMMENT '创建人',
+    `cst_create`    datetime    NOT NULL DEFAULT '2000-01-01 00:00:01' COMMENT '创建时间',
+    `modifier`      varchar(32) NOT NULL DEFAULT '' COMMENT '修改人',
+    `cst_modify`    datetime    NOT NULL DEFAULT '2000-01-01 00:00:01' COMMENT '修改时间',
     PRIMARY KEY (`id`),
     UNIQUE KEY `uk_code` (`code`) USING BTREE,
-    KEY `idx_tenant_biz_func_user` (`tenant_code`, `biz_code`, `function_code`, `user_identity`) USING BTREE,
-    KEY `idx_expire` (`cst_expire`) USING BTREE
+    KEY             `idx_tenant_biz_func_user` (`tenant_code`, `biz_code`, `function_code`, `user_identity`) USING BTREE,
+    KEY             `idx_expire` (`cst_expire`) USING BTREE
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_unicode_ci COMMENT ='用户通知验证码表';
